3660b5513c2d88aa6ebe5dc93029ef5e277d3411
Patrick Berger <patrick.berger256@gmail.com>
2024-07-17 17:11:27 +0200

    added return types throughout

--- data_generation/anderson_model.jl
+++ data_generation/anderson_model.jl
@@ -30,7 +30,7 @@ struct AndersonCore
     hopping_operator::Matrix{Operator}
     coulomb_operator::Operator
 
-    function AndersonCore(nbath::Int)
+    function AndersonCore(nbath::Int)::AndersonCore
         nsites = nbath + 1 # because we have one impurity
         nspins = 2 # since we have spin-1/2 particles, there are only two different spin projections
         fockspace = FockSpace(nspins * nsites)
@@ -46,25 +46,25 @@ end
 
 
 """Return the annihilator operator of the Anderson core."""
-function annihilator_operator(core::AndersonCore)
+function annihilator_operator(core::AndersonCore)::Matrix{Operator}
     return core.annihilator_operator
 end
 
 
 """Return the number of bath sites of the Anderson core (includes the impurity site)."""
-function nbath(core::AndersonCore)
+function nbath(core::AndersonCore)::Int
     return core.nsites - 1 # -1 becaue we don't want to count the impurity site
 end
 
 
 """Return the number of sites of the Anderson core (includes the impurity site)."""
-function nsites(core::AndersonCore)
+function nsites(core::AndersonCore)::Int
     return core.nsites
 end
 
 
 """Return the number of different spin projections of the particles in the Anderson core."""
-function nspins(core::AndersonCore)
+function nspins(core::AndersonCore)::Int
     return core.nspins
 end
 
@@ -106,20 +106,20 @@ struct AndersonParameters
     v::AbstractVector{Float64} # hopping amplitude
     β::Float64 # 1 / temperature
 
-    function AndersonParameters(u::Float64, ε_imp::Float64, ε::AbstractVector{Float64}, v::AbstractVector{Float64}, β::Float64)
+    function AndersonParameters(u::Float64, ε_imp::Float64, ε::AbstractVector{Float64}, v::AbstractVector{Float64}, β::Float64)::AndersonParameters
         length(v) != length(ε) && throw(DomainError("There must be as many energies ($(length(ε))) as hopping amplitudes ($(length(v))."))
         return new(u, ε_imp, ε, v, β)
     end
 
-    function AndersonParameters(u::Float64, ε_imp::Float64, ε::Float64, v::AbstractVector{Float64}, β::Float64)
+    function AndersonParameters(u::Float64, ε_imp::Float64, ε::Float64, v::AbstractVector{Float64}, β::Float64)::AndersonParameters
         return AndersonParameters(u, ε_imp, fill(ε, (length(v),)), v, β)
     end
 
-    function AndersonParameters(u::Float64, ε_imp::Float64, ε::AbstractVector{Float64}, v::Float64, β::Float64)
+    function AndersonParameters(u::Float64, ε_imp::Float64, ε::AbstractVector{Float64}, v::Float64, β::Float64)::AndersonParameters
         return AndersonParameters(u, ε_imp, ε, fill(v, (length(ε),)), β)
     end
 
-    function AndersonParameters(u::Float64, ε_imp::Float64, ε::Float64, v::Float64, β::Float64)
+    function AndersonParameters(u::Float64, ε_imp::Float64, ε::Float64, v::Float64, β::Float64)::AndersonParameters
         return AndersonParameters(u, ε_imp, fill(ε, (1,)), fill(v, (1,)), β)
     end
 
@@ -127,13 +127,13 @@ end
 
 
 """Return the number of bath sites of the corresponding Anderson model (includes the impurity site)."""
-function nbath(parameters::AndersonParameters)
+function nbath(parameters::AndersonParameters)::Int
     return length(parameters.ε)
 end
 
 
 """Return the number of sites of the corresponding Anderson model (includes the impurity site)."""
-function nsites(parameters::AndersonParameters)
+function nsites(parameters::AndersonParameters)::Int
     return nbath(parameters) + 1 # +1 because we have one impurity site
 end
 
@@ -150,7 +150,7 @@ end
 Compute the non interacting Hamltonian for an Anderson model
 c[1, σ] are the anihilators of the impurity site per convention.
 """
-function non_interacting_hamiltonian(core::AndersonCore, parameters::AndersonParameters)
+function non_interacting_hamiltonian(core::AndersonCore, parameters::AndersonParameters)::Operator{Float64}
     nsites(core) != nsites(parameters) && throw(DomainError("The number of sites has to match; $(nsites(core)) (core) vs. $(nsites(parameters)) (parameters)."))
 
     H = Operator(core.fockspace)
@@ -183,7 +183,7 @@ end
 
 
 """Return the hamilton operator of the Anderson model."""
-function hamiltonian(core::AndersonCore, parameters::AndersonParameters)
+function hamiltonian(core::AndersonCore, parameters::AndersonParameters)::Operator{Float64}
     # be aware: there is a (small) difference between 0 * operator and no operator
     # to not end up with extremly small numbers instead of 0, do it this way
     return if parameters.u != 0
@@ -195,19 +195,19 @@ end
 
 
 """Compute the non-interacting hamiltonian for the given Anderson model as required by full_tau and full_freq."""
-function non_interacting_hamiltonian_eigen(core::AndersonCore, parameters::AndersonParameters)
+function non_interacting_hamiltonian_eigen(core::AndersonCore, parameters::AndersonParameters)::EigenPartition{Float64}
     return Fermions.Propagators.HamiltonianEigen(non_interacting_hamiltonian(core, parameters), core.quantum_numbers, parameters.β)
 end
 
 
 """Compute the full hamiltonian for the given Anderson model as required by full_tau and full_freq."""
-function full_hamiltonian_eigen(core::AndersonCore, parameters::AndersonParameters)
+function full_hamiltonian_eigen(core::AndersonCore, parameters::AndersonParameters)::EigenPartition{Float64}
     return Fermions.Propagators.HamiltonianEigen(hamiltonian(core, parameters), core.quantum_numbers, parameters.β)
 end
 
 
 """Compute the non-interacting imaginary times Green's for the given Anderson model."""
-function greens_operators(site::NTuple{2,Int64}, core::AndersonCore)
+function greens_operators(site::NTuple{2,Int64}, core::AndersonCore)::Tuple{Operator,Operator}
     return (-annihilator_operator(core)[site[1], 1], annihilator_operator(core)[site[2], 1]')
 end
 
@@ -252,25 +252,25 @@ end
 
 
 """Compute the non-interacting Matsubara Green's function for the given Anderson model."""
-function g0_freq(site::NTuple{2,Int64}, frequencies::AbstractVector{FermionicFreq}, core::AndersonCore, parameters::AndersonParameters)
+function g0_freq(site::NTuple{2,Int64}, frequencies::AbstractVector{FermionicFreq}, core::AndersonCore, parameters::AndersonParameters)::AbstractVector{ComplexF64}
     return Fermions.Propagators.full_freq(greens_operators(site, core), frequencies, non_interacting_hamiltonian_eigen(core, parameters), parameters.β)
 end
 
 
 """Compute the Matsubara Green's function for the given Anderson model."""
-function g_freq(site::NTuple{2,Int64}, frequencies::AbstractVector{FermionicFreq}, core::AndersonCore, parameters::AndersonParameters)
+function g_freq(site::NTuple{2,Int64}, frequencies::AbstractVector{FermionicFreq}, core::AndersonCore, parameters::AndersonParameters)::AbstractVector{ComplexF64}
     return Fermions.Propagators.full_freq(greens_operators(site, core), frequencies, full_hamiltonian_eigen(core, parameters), parameters.β)
 end
 
 
 """Compute the Matsubara self energies for a given Anderson model."""
-function self_energies(site::NTuple{2,Int64}, frequencies::AbstractVector{FermionicFreq}, core::AndersonCore, parameters::AndersonParameters)
+function self_energies(site::NTuple{2,Int64}, frequencies::AbstractVector{FermionicFreq}, core::AndersonCore, parameters::AndersonParameters)::AbstractVector{ComplexF64}
     return 1 ./ g0_freq(site, frequencies, core, parameters) - 1 ./ g_freq(site, frequencies, core, parameters)
 end
 
 
 """Compute the expectation value of the number operator."""
-function number_operator_expectation(site::NTuple{2,Int64}, τ::AbstractVector{Float64}, core::AndersonCore, parameters::AndersonParameters)
+function number_operator_expectation(site::NTuple{2,Int64}, τ::AbstractVector{Float64}, core::AndersonCore, parameters::AndersonParameters)::Float64
     return -g_tau(site, τ, core, parameters)[1] # the size of the list is just one, so we just take the first entry
 end
 
